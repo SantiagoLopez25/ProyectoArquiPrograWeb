fd527d0e17d09e93bdc63bd0014146ad351f5afa
SantiagoLopez25 <msantiagolopez@umes.edu.gt>
2024-11-08 21:36:43 -0600

    Correccion

--- db/storeonline.sql
+++ db/storeonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2024 a las 16:16:56
+-- Tiempo de generación: 09-11-2024 a las 04:34:30
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -13,6 +13,10 @@ SET time_zone = "+00:00";
 
 CREATE DATABASE IF NOT EXISTS `storeonline`;
 USE `storeonline`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `storeonline`
