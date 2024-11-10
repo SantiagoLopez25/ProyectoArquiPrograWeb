fbeea13005be550270c801c4ced88d637e11619e
wil <wil@debian>
2024-11-10 13:13:54 -0600

    update

--- src/frontend/src/components/Footer.vue
+++ src/frontend/src/components/Footer.vue
@@ -58,7 +58,7 @@ export default {
         <div class="container relative">
 
             <div class="sofa-img">
-                <img src="@/assets/images/sofa.png" alt="Image" class="img-fluid">
+                <img src="@/assets/images/camara.png" alt="Image" class="img-fluid">
             </div>
 
             <div class="row">
@@ -66,14 +66,14 @@ export default {
                     <div class="subscription-form">
                         <h3 class="d-flex align-items-center"><span class="me-1"><img
                                     src="@/assets/images/envelope-outline.svg" alt="Image"
-                                    class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+                                    class="img-fluid"></span><span>¡Suscríbete a nuestro Newsletter!</span></h3>
 
                         <form action="#" class="row g-3">
                             <div class="col-auto">
-                                <input type="text" class="form-control" placeholder="Enter your name">
+                                <input type="text" class="form-control" placeholder="Ingresa tu nombre">
                             </div>
                             <div class="col-auto">
-                                <input type="email" class="form-control" placeholder="Enter your email">
+                                <input type="email" class="form-control" placeholder="Ingresa tu email">
                             </div>
                             <div class="col-auto">
                                 <button class="btn btn-primary">
@@ -89,9 +89,8 @@ export default {
             <div class="row g-5 mb-5">
                 <div class="col-lg-4">
                     <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">{{ this.$APP_NAME }}<span></span></a></div>
-                    <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
-                        malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
-                        Pellentesque habitant</p>
+                    <p class="mb-4">¿Cómo ha sido tu experiencia con el sitio?
+                        Danos tu opinión acerca de nuestro sitio web</p>
 
                     <ul class="list-unstyled custom-social">
                         <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
@@ -105,54 +104,55 @@ export default {
                     <div class="row links-wrap">
                         <div class="col-6 col-sm-6 col-md-3">
                             <ul class="list-unstyled">
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Contact us</a></li>
+                                <li><b>Nuestra Tineda</b></li>
+                                <li><RouterLink  to="/about">Sobre nosotros</RouterLink></li>
+                                <li><RouterLink to="/services">Servicios</RouterLink ></li>
+                                
+                            </ul>
+                        </div>
+
+                       
+                        <div class="col-6 col-sm-6 col-md-3">
+                            <ul class="list-unstyled">
+                                
+                                <li><b>Soporte</b></li>
+                                <li><a href="#">Sucursales</a></li>
+                                <li><a href="#">Redes sociales</a></li>
                             </ul>
                         </div>
 
                         <div class="col-6 col-sm-6 col-md-3">
                             <ul class="list-unstyled">
-                                <li><a href="#">Support</a></li>
-                                <li><a href="#">Knowledge base</a></li>
-                                <li><a href="#">Live chat</a></li>
+                                <li><b>Catálogos</b></li>
+                                <li><RouterLink  to="/shop">Comprar</RouterLink></li>
+                                <li><RouterLink to="/blog">Blog</RouterLink></li>
                             </ul>
                         </div>
 
-                        <div class="col-6 col-sm-6 col-md-3">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Jobs</a></li>
-                                <li><a href="#">Our team</a></li>
-                                <li><a href="#">Leadership</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-
+                        <!--
                         <div class="col-6 col-sm-6 col-md-3">
                             <ul class="list-unstyled">
                                 <li><a href="#">Nordic Chair</a></li>
                                 <li><a href="#">Kruzo Aero</a></li>
                                 <li><a href="#">Ergonomic Chair</a></li>
                             </ul>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
 
             </div>
-
-            <div class="border-top copyright">
+            <!--            <div class="border-top copyright">
                 <div class="row pt-4">
                     <div class="col-lg-6 text-center text-lg-end">
                         <ul class="list-unstyled d-inline-flex ms-auto">
-                            <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
+                            <li class="me-4"><a href="#">Términos &amp; Condiciones</a></li>
+                            <li><a href="#">Política de privacidad</a></li>
                         </ul>
                     </div>
 
                 </div>
             </div>
-
+            -->
         </div>
     </footer>
     <!-- End Footer Section -->
