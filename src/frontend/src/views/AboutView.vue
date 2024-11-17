5570ee9926ffdeb3973b5f9ac46bfb122ea880de
wil <wil@debian>
2024-11-16 20:18:27 -0600

    vistas

--- src/frontend/src/views/AboutView.vue
+++ src/frontend/src/views/AboutView.vue
@@ -2,10 +2,11 @@
 import { RouterLink } from 'vue-router'
 import Footer from '@/components/Footer.vue';
 import Header from '@/components/Header.vue';
+import Testimonials from '@/components/Testimonials.vue';
 
 export default {
   page: { },
-  components: {Header, Footer },
+  components: {Header, Footer, Testimonials },
   data() {
     return {
       
@@ -54,57 +55,16 @@ export default {
     <div class="container">
       <div class="row justify-content-between align-items-center">
         <div class="col-lg-6">
-          <h2 class="section-title">Why Choose Us</h2>
-          <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet
-            dolor tempor tristique.</p>
+          <h2 class="section-title">¿Por qué elegirnos?</h2>
+          <p>{{  this.$APP_NAME }} tiene un equipo capacitado para brindarle soporte profesional para ayudarle 
+            con sus equipo, estamos comprometidos a ofrecerle el mejor servicio. .</p>
 
-          <div class="row my-5">
-            <div class="col-6 col-md-6">
-              <div class="feature">
-                <div class="icon">
-                  <img src="@/assets/images/truck.svg" alt="Image" class="imf-fluid">
-                </div>
-                <h3>Fast &amp; Free Shipping</h3>
-                <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
-              </div>
-            </div>
-
-            <div class="col-6 col-md-6">
-              <div class="feature">
-                <div class="icon">
-                  <img src="@/assets/images/bag.svg" alt="Image" class="imf-fluid">
-                </div>
-                <h3>Easy to Shop</h3>
-                <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
-              </div>
-            </div>
-
-            <div class="col-6 col-md-6">
-              <div class="feature">
-                <div class="icon">
-                  <img src="@/assets/images/support.svg" alt="Image" class="imf-fluid">
-                </div>
-                <h3>24/7 Support</h3>
-                <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
-              </div>
-            </div>
-
-            <div class="col-6 col-md-6">
-              <div class="feature">
-                <div class="icon">
-                  <img src="@/assets/images/return.svg" alt="Image" class="imf-fluid">
-                </div>
-                <h3>Hassle Free Returns</h3>
-                <p>Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate.</p>
-              </div>
-            </div>
-
-          </div>
+            
         </div>
 
         <div class="col-lg-5">
           <div class="img-wrap">
-            <img src="@/assets/images/why-choose-us-img.jpg" alt="Image" class="img-fluid">
+            <img src="https://t4.ftcdn.net/jpg/05/59/14/65/360_F_559146542_6U4XiJlJZ8iui6ye1D9H8Cctfd3Z3vGa.jpg" alt="Image" class="img-fluid">
           </div>
         </div>
 
@@ -114,7 +74,7 @@ export default {
   <!-- End Why Choose Us Section -->
 
   <!-- Start Team Section -->
-  <div class="untree_co-section">
+  <!-- <div class="untree_co-section">
     <div class="container">
 
       <div class="row mb-5">
@@ -125,7 +85,7 @@ export default {
 
       <div class="row">
 
-        <!-- Start Column 1 -->
+        <!-- Start Column 1 --
         <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
           <img src="@/assets/images/person_1.jpg" class="img-fluid mb-5">
           <h3 clas><a href="#"><span class="">Lawson</span> Arnold</a></h3>
@@ -134,9 +94,9 @@ export default {
             Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
           <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
         </div>
-        <!-- End Column 1 -->
+        <!-- End Column 1 --
 
-        <!-- Start Column 2 -->
+        <!-- Start Column 2 --
         <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
           <img src="@/assets/images/person_2.jpg" class="img-fluid mb-5">
 
@@ -147,9 +107,9 @@ export default {
           <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
 
         </div>
-        <!-- End Column 2 -->
+        <!-- End Column 2 --
 
-        <!-- Start Column 3 -->
+        <!-- Start Column 3 --
         <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
           <img src="@/assets/images/person_3.jpg" class="img-fluid mb-5">
           <h3 clas><a href="#"><span class="">Patrik</span> White</a></h3>
@@ -158,9 +118,9 @@ export default {
             Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
           <p class="mb-0"><a href="#" class="more dark">Learn More <span class="icon-arrow_forward"></span></a></p>
         </div>
-        <!-- End Column 3 -->
+        <!-- End Column 3 --
 
-        <!-- Start Column 4 -->
+        <!-- Start Column 4 --
         <div class="col-12 col-md-6 col-lg-3 mb-5 mb-md-0">
           <img src="@/assets/images/person_4.jpg" class="img-fluid mb-5">
 
@@ -172,122 +132,19 @@ export default {
 
 
         </div>
-        <!-- End Column 4 -->
+        <!-- End Column 4 --
 
 
 
       </div>
     </div>
-  </div>
+  </div> -->
   <!-- End Team Section -->
 
 
 
   <!-- Start Testimonial Slider -->
-  <div class="testimonial-section before-footer-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-7 mx-auto text-center">
-          <h2 class="section-title">Testimonials</h2>
-        </div>
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-lg-12">
-          <div class="testimonial-slider-wrap text-center">
-
-            <div id="testimonial-nav">
-              <span class="prev" data-controls="prev"><span class="fa fa-chevron-left"></span></span>
-              <span class="next" data-controls="next"><span class="fa fa-chevron-right"></span></span>
-            </div>
-
-            <div class="testimonial-slider">
-
-              <div class="item">
-                <div class="row justify-content-center">
-                  <div class="col-lg-8 mx-auto">
-
-                    <div class="testimonial-block text-center">
-                      <blockquote class="mb-5">
-                        <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
-                          malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
-                          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                          Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                      </blockquote>
-
-                      <div class="author-info">
-                        <div class="author-pic">
-                          <img src="@/assets/images/person-1.png" alt="Maria Jones" class="img-fluid">
-                        </div>
-                        <h3 class="font-weight-bold">Maria Jones</h3>
-                        <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-              <!-- END item -->
-
-              <div class="item">
-                <div class="row justify-content-center">
-                  <div class="col-lg-8 mx-auto">
-
-                    <div class="testimonial-block text-center">
-                      <blockquote class="mb-5">
-                        <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
-                          malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
-                          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                          Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                      </blockquote>
-
-                      <div class="author-info">
-                        <div class="author-pic">
-                          <img src="@/assets/images/person-1.png" alt="Maria Jones" class="img-fluid">
-                        </div>
-                        <h3 class="font-weight-bold">Maria Jones</h3>
-                        <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-              <!-- END item -->
-
-              <div class="item">
-                <div class="row justify-content-center">
-                  <div class="col-lg-8 mx-auto">
-
-                    <div class="testimonial-block text-center">
-                      <blockquote class="mb-5">
-                        <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus
-                          malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
-                          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                          Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
-                      </blockquote>
-
-                      <div class="author-info">
-                        <div class="author-pic">
-                          <img src="@/assets/images/person-1.png" alt="Maria Jones" class="img-fluid">
-                        </div>
-                        <h3 class="font-weight-bold">Maria Jones</h3>
-                        <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-              <!-- END item -->
-
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Testimonials></Testimonials>
   <!-- End Testimonial Slider -->
 
    <Footer></Footer>
